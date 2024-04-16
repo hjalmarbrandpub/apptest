fd2baff259efb3e24a00372ca8e02b38717761f4
hjalmarbrandpub <130356548+hjalmarbrandpub@users.noreply.github.com>
2024-04-16 14:28:22 +0000

    Added docstring to app

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ Calculator library containing basic math operations.
 
 
 def add(first_term, second_term):
+   
     return first_term + second_term
 
 
 def subtract(first_term, second_term):
+     """Return the difference of the inputs."""
     return first_term - second_term
