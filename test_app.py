e86a21f2b56dfdb90affa9d068b8d4e57a35e941
hjalmarbrandpub <130356548+hjalmarbrandpub@users.noreply.github.com>
2024-04-16 14:48:39 +0000

    added checks

--- test_app.py
+++ test_app.py
@@ -6,9 +6,11 @@ import app
 
 
 class TestCalculator:
-
+    """Define tests"""
     def test_addition(self):
+        """Test sum"""
         assert 4 == app.add(2, 2)
 
     def test_subtraction(self):
+        """Test subtraction"""
         assert 2 == app.subtract(4, 2)
