eb8d5434a24817c6fe713d85e3ddd7b38f0bfb3f
hjalmarbrandpub <130356548+hjalmarbrandpub@users.noreply.github.com>
2024-04-16 14:30:01 +0000

    Modified App

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ Calculator library containing basic math operations.
 
 
 def add(first_term, second_term):
-   
+    """Return the sum of the inputs."""
     return first_term + second_term
 
-
 def subtract(first_term, second_term):
-     """Return the difference of the inputs."""
+    """Return the difference of the inputs."""
     return first_term - second_term
