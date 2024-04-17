b9f847d4d5446204da49c66ccf7d91877e1ccbfe
hjalmarbrandpub <130356548+hjalmarbrandpub@users.noreply.github.com>
2024-04-17 10:18:29 +0000

    added setup.py for automation

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0.1'
+DESCRIPTION = 'A simple calculator app package'
+LONG_DESCRIPTION = 'A package that can add or subtract two values'
+
+setup(
+    name="hb-calc-app",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Hjalmar Brand",
+    author_email="myemai@mail.com",
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+    keywords='calculator',
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+    ]
+)
