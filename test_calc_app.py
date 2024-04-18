3a3da3e47d054c9207627efdfe6b9f0f960081e5
hjalmarbrandpub <130356548+hjalmarbrandpub@users.noreply.github.com>
2024-04-18 10:15:41 +0000

    changed test file

--- test_calc_app.py
+++ test_calc_app.py
@@ -15,5 +15,5 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(subtract(5, -3), 8)
         self.assertEqual(subtract(0, 0), 0)
 
-if __name__ == '__main__':
-    unittest.main()
+#if __name__ == '__main__':
+#    unittest.main()
